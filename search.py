21e4633ac489278764467f7e9539f88a3d157178
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-02-20 19:38:00 +0900

    検索結果をcsvに書き出し出来るよう変更

--- search.py
+++ search.py
@@ -2,17 +2,11 @@ from write_csv import CinderellaWriteCsv
 from get_twitter import url, twitter
 import json
 
-list_sample = []
-
-list_sample.append({'a':10, 'b':20, 'c':30})
-list_sample.append({'a':11, 'b':22, 'c':33})
-
-cin_csv = CinderellaWriteCsv('./sample.csv', list_sample[0].keys())
-
-cin_csv.add_columns(list_sample)
-
 # Enedpointへ渡すパラメーター
 keyword = '桐生つかさ exclude:retweets'
+
+header = ['user_id', 'id', 'created_at', 'text']
+cinderella_csv = CinderellaWriteCsv('./sample.csv', header)
 
 params ={
          'count' : 100,      # 取得するtweet数
@@ -24,10 +18,17 @@ req = twitter.get(url, params = params)
 
 if req.status_code == 200:
 	res = json.loads(req.text)
+	list_for_add_csv =[]
 	for line in res['statuses']:
-		print('id = ' + line['id_str'] + ' user_id = ' + line['user']['id_str'] + ' datetime = ' + line['created_at'])
-		print(line['text'])
-		print('*******************************************')
-		
+		dict_of_column = {}
+		# tweet_id, created_at, textを取得
+		for i in range(1, len(header)):
+			dict_of_column[header[i]] = line[header[i]]
+		# user_idを取得
+		dict_of_column[header[0]] = line['user']['id'] 
+		list_for_add_csv.append(dict_of_column)
+
+	cinderella_csv.add_columns(list_for_add_csv)
+
 else:
 	print("Failed: %d" % req.status_code)
