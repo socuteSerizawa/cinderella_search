3b6364a1c23d21921e7da5d1b3ab03693d2b3c96
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-02-21 14:28:18 +0900

    CSVに自動保存する機能の追加

--- search.py
+++ search.py
@@ -1,34 +1,62 @@
 from write_csv import CinderellaWriteCsv
 from get_twitter import url, twitter
 import json
+import time
 
 # Enedpointへ渡すパラメーター
-keyword = '桐生つかさ exclude:retweets'
+keyword = 'Violet Violence exclude:retweets'
 
 header = ['user_id', 'id', 'created_at', 'text']
 cinderella_csv = CinderellaWriteCsv('./sample.csv', header)
 
+MAX_ID = -1
+SINCE_ID = -1
+MAX_TWEETS_ONETIME = 100
+
 params ={
-         'count' : 100,      # 取得するtweet数
-         'q'     : keyword,  # 検索キーワード
-         'max_id': None,     # 取得する最大tweetID
+         'count'   : MAX_TWEETS_ONETIME, # 取得するtweet数
+         'q'       : keyword,    # 検索キーワード
+         'max_id'  : MAX_ID,     # 取得する最大tweetID
+         'since_id': SINCE_ID,   # このIDより大きいtweetIDを取得
          }
 
-req = twitter.get(url, params = params)
+MAX_ROOP = 100
 
-if req.status_code == 200:
-	res = json.loads(req.text)
-	list_for_add_csv =[]
-	for line in res['statuses']:
-		dict_of_column = {}
-		# tweet_id, created_at, textを取得
-		for i in range(1, len(header)):
-			dict_of_column[header[i]] = line[header[i]]
-		# user_idを取得
-		dict_of_column[header[0]] = line['user']['id'] 
-		list_for_add_csv.append(dict_of_column)
+roop_count = 0
+count_tweets_for_debug = 0
+while(roop_count < MAX_ROOP):
+	# リクエストクエリの送信
+	req = twitter.get(url, params = params)
 
-	cinderella_csv.add_columns(list_for_add_csv)
+	if req.status_code == 200:
+		res = json.loads(req.text)
 
-else:
-	print("Failed: %d" % req.status_code)
+		# 受信結果から，必要なカラムのピックアップ
+		list_for_add_csv =[]
+		for column_tweet in res['statuses']:
+			dict_of_column = {}
+			# tweet_id, created_at, textを取得
+			for i in range(1, len(header)):
+				dict_of_column[header[i]] = column_tweet[header[i]]
+			# user_idを取得
+			dict_of_column[header[0]] = column_tweet['user']['id'] 
+			list_for_add_csv.append(dict_of_column)
+			# 取得したtweet数の記録
+			count_tweets_for_debug += 1
+		# CSVに保存
+		cinderella_csv.add_columns(list_for_add_csv)
+		# 取得する最大tweet_idを更新
+		params['max_id'] = list_for_add_csv[-1]['id'] - 1
+		# 取得したtweet数の確認
+		print(count_tweets_for_debug)
+		# 受信結果数により，これ以上取得可能なtweetがあるかどうかを判定
+		if(len(list_for_add_csv) < MAX_TWEETS_ONETIME):
+			break
+
+	else:
+		print("Failed: %d" % req.status_code)
+
+	roop_count += 1
+	time.sleep(1)
+
+print('record tweets = '+ str(count_tweets_for_debug))
